1d861eef446768f043872f536e00bc220ef4d531
vahid torkaman <vahidt@spotify.com>
2024-06-17 16:37:56 +0200

    sdk id flutter for swift

--- ios/Classes/ConfidenceFlutterSdkPlugin.swift
+++ ios/Classes/ConfidenceFlutterSdkPlugin.swift
@@ -15,7 +15,7 @@ public class ConfidenceFlutterSdkPlugin: NSObject, FlutterPlugin {
         case "setup":
             let apiKey = call.arguments as! String
             self.confidence = Confidence.Builder(clientSecret: apiKey)
-                .sdkId("SDK_ID_SWIFT_CONFIDENCE")
+                .sdkId("SDK_ID_FLUTTER_IOS_CONFIDENCE")
                 .sdkVersion("0.0.1")
                 .build()
             result("")
