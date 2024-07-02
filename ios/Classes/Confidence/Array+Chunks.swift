e41fda812f466d33e3dde9d7f3792fdc2edc27e0
vahid torkaman <vahidt@spotify.com>
2024-07-02 14:21:06 +0200

    use submodule to update the ios part of the sdk

--- ios/Classes/Confidence/Array+Chunks.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension Array {
-    func chunk(size: Int) -> [[Element]] {
-        return stride(from: 0, to: count, by: size).map {
-            Array(self[$0..<Swift.min($0 + size, count)])
-        }
-    }
-}
--- ios/Classes/Confidence/Resolver.swift
+++
@@ -1,9 +0,0 @@
-public protocol Resolver {
-    // This throws if the requested flag is not found
-    func resolve(flag: String, contextHash: String) throws -> ResolveResult
-}
-
-public struct ResolveResult {
-    var resolvedValue: ResolvedValue
-    var resolveToken: String?
-}
