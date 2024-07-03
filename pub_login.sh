c593059f5edef92fbe46b4fb36ca3b937d784b2b
vahid torkaman <vahidt@spotify.com>
2024-07-03 13:53:04 +0200

    add release please

---
+++ pub_login.sh
@@ -0,0 +1,35 @@
+# This script creates/updates credentials.json file which is used
+# to authorize publisher when publishing packages to pub.dev
+
+# Checking whether the secrets are available as environment
+# variables or not.
+if [ -z "${PUB_ACCESS_TOKEN" ]; then
+  echo "Missing PUB_ACCESS_TOKEN environment variable"
+  exit 1
+fi
+
+if [ -z "${PUB_REFRESH_TOKEN}" ]; then
+  echo "Missing PUB_REFRESH_TOKEN environment variable"
+  exit 1
+fi
+
+if [ -z "${PUB_TOKEN_ENDPOINT}" ]; then
+  echo "Missing PUB_TOKEN_ENDPOINT environment variable"
+  exit 1
+fi
+
+if [ -z "${PUB_EXPIRATION}" ]; then
+  echo "Missing PUB_EXPIRATION environment variable"
+  exit 1
+fi
+
+# Create credentials.json file.
+cat <<EOF > ~/.pub-cache/credentials.json
+{
+  "accessToken":"${PUB_ACCESS_TOKEN}",
+  "refreshToken":"${PUB_REFRESH_TOKEN}",
+  "tokenEndpoint":"${PUB_TOKEN_ENDPOINT}",
+  "scopes":["https://www.googleapis.com/auth/userinfo.email","openid"],
+  "expiration":${PUB_EXPIRATION}
+}
+EOF
