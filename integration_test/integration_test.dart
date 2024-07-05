da1baa08711a301df674a05c848500bb06fb364c
vahid torkaman <vahidt@spotify.com>
2024-07-05 14:40:43 +0200

    write basic test for integration test of example app

---
+++ integration_test/integration_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:confidence_flutter_sdk/confidence_flutter_sdk.dart'; // Adjust the import to your actual plugin
+
+void main() {
+  group('Confidence', () {
+
+    });
+  });
+}
